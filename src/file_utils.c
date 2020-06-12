00c88950a302dc096d2012f626e5d160fe3459d3
Mihnea-Teodor Savin <ms3319@texel01.doc.ic.ac.uk>
2020-06-12 11:01:20 +0100

    string replacer

--- src/file_utils.c
+++ src/file_utils.c
@@ -1,5 +1,5 @@
 #include "file_utils.h"
-
+#include "string_utils.h"
 #include <stdlib.h>
 
 // Remember to free the returned buffer if non null!
@@ -12,3 +12,18 @@ char* read_file_into_buffer(FILE* file, long* filelen) {
 	fread(ret, *filelen, 1, file);
 	return ret;
 }
+
+void replace_strings(FILE* fp, char* rep, char* with) {
+  fseek(fp, 0, SEEK_END);
+  long long file_length = ftell(fp);
+  fseek(fp,0,SEEK_SET);
+  char* file_text = malloc(file_length + 1);
+  fread(file_text,file_length,1,fp);
+  char* final_text = str_replace(file_text,rep,with);
+  if(fputs(final_text, fp) <= 0) {
+    perror("Couldn't print to file!");
+    exit(EXIT_FAILURE); 
+  }
+  fclose(fp);
+  return;  
+}	
