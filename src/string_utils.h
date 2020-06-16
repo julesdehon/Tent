f5a1bd6be4505c039d5fa03e2bf72efb1e1f13ed
Jules Dehon <jd2319@texel22.doc.ic.ac.uk>
2020-06-16 11:05:56 +0100

    Changed function signature of str_replace to take const *char, and added file tree walking function to delete a directory in tent.c

--- src/string_utils.h
+++ src/string_utils.h
@@ -1,7 +1,7 @@
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
 
-char* str_replace(char* orig, char* rep, char* with);
+char* str_replace(const char* orig, char* rep, char* with);
 
 char* trim_whitespace(char* orig);
 
--- src/tent.c
+++ src/tent.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 500 // used by ftw.h
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +7,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <ftw.h>
 
 #include "string_utils.h"
 #include "file_utils.h"
@@ -27,8 +30,7 @@
 #define DIR_THEME "/theme"
 #define FIL_CONFIG "/config.tent"
 
-void build_site_aux(const char *content_path, const char *public_path, VariableMap *config_map, TemplateMap *template_map)
-{
+void build_site_aux(const char *content_path, const char *public_path, VariableMap *config_map, TemplateMap *template_map) {
   DIR *dir;
   struct dirent *entry;
 
@@ -77,7 +79,7 @@ void build_site_aux(const char *content_path, const char *public_path, VariableM
 	  perror("tent.c - error creating file for copying to public directory");
 	  exit(EXIT_FAILURE);
 	}
-	copy_file(f, out); // <-- this should be a new file util to implement
+	copy_file(f, out);
 	fclose(out);
       }
       fclose(f);
@@ -85,6 +87,21 @@ void build_site_aux(const char *content_path, const char *public_path, VariableM
   }
   closedir(dir);
 }
+
+int remove_file_callback(const char *path, const struct stat *stat_buffer, int typeflag, struct FTW *ftwbuf){
+  int rv = remove(path);
+  if (rv)
+    perror(path);
+  return rv;
+}
+
+int del_directory(char *path) {
+  return nftw(path, remove_file_callback, 64, FTW_DEPTH | FTW_PHYS);
+}
+
+/* int copy_file_callback(const char *path, const struct stat *stat_buffer, int typeflag, struct FTW *ftwbuf) { */
+  
+/* } */
 
 void print_help() {
   printf("\n%s\n", HELP_MESSAGE);
@@ -94,12 +111,7 @@ void build_site() {
   VariableMap *config_map = init_variable_map();
   load_config("config.tent", config_map);
   TemplateMap *template_map = load_template_map();
-  DIR *dir;
-  if ((dir = opendir("public"))) {
-    closedir(dir);
-    printf("Please delete the public folder before building.\n");
-    return;
-  }
+  del_directory("public");
   mkdir("public", DIR_PERMS);
   build_site_aux("content", "public", config_map, template_map);
 }
